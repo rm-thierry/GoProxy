bb3a7a7ef3ea7ce3348155c772e5ea9ed37db680
Retaxo <105597296+sudo-Tiger@users.noreply.github.com>
2024-02-23 22:18:05 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func sendProxyProtocol(clientConn net.Conn, serverOutput io.Writer) {
+	sourceAddress := clientConn.RemoteAddr().(*net.TCPAddr)
+	destAddress := clientConn.LocalAddr().(*net.TCPAddr)
+
+	proxyProtocolHeader := fmt.Sprintf("PROXY TCP4 %s %s %d %d\r\n",
+		sourceAddress.IP.String(), destAddress.IP.String(), sourceAddress.Port, destAddress.Port)
+
+	serverOutput.Write([]byte(proxyProtocolHeader))
+}
+
+func readAndHandleCommands(clientConn net.Conn) {
+	reader := bufio.NewReader(clientConn)
+	for {
+		command, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading command:", err)
+			return
+		}
+
+		switch strings.TrimSpace(command) {
+		case "help":
+			fmt.Fprintln(clientConn, "Available commands: help, status, etc.")
+		default:
+			fmt.Fprintln(clientConn, "Unknown command. Type 'help' for available commands.")
+		}
+	}
+}
+
+func readConsoleCommands() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("\033[0;36mPROXY: \033[0m")
+		scanner.Scan()
+		command := strings.TrimSpace(scanner.Text())
+		switch command {
+		case "help":
+			fmt.Println("Alle Verfügbaren Commands")
+			fmt.Println("/help: Show this Menu")
+			fmt.Println("/clear: Clear the Console")
+		case "stop":
+			fmt.Println("Server shutting Down")
+			os.Exit(0)
+		case "clear":
+			clearConsole()
+		default:
+		}
+	}
+}
+
+func clearConsole() {
+	cmd := exec.Command("clear")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+
+func handleConnection(clientConn net.Conn, minecraftServer string, minecraftPort string) {
+	serverConn, err := net.Dial("tcp", fmt.Sprintf("%s:%s", minecraftServer, minecraftPort))
+	if err != nil {
+		fmt.Println("Error connecting to Minecraft server:", err)
+		return
+	}
+	defer serverConn.Close()
+	sendProxyProtocol(clientConn, serverConn)
+	go readAndHandleCommands(clientConn)
+	go func() {
+		_, err := io.Copy(serverConn, clientConn)
+		if err != nil {
+			fmt.Println("Error forwarding data to Minecraft server:", err)
+		}
+	}()
+
+	_, err = io.Copy(clientConn, serverConn)
+	if err != nil {
+		fmt.Println("Error forwarding data to client:", err)
+	}
+}
+
+func main() {
+	minecraftServer := "78.46.208.71"
+	minecraftPort := "25565"
+	port := 25555
+	fmt.Println("\u001B[34m  _   _ ______ _______      ________ ")
+	fmt.Println("\u001B[34m | \\ | |  ____|  __ \\ \\    / /  ____|")
+	fmt.Println("\u001B[34m |  \\| | |__  | |__) \\ \\  / /| |__  ")
+	fmt.Println("\u001B[34m | . ` |  __| |  _  / \\ \\/ / |  __| ")
+	fmt.Println("\u001B[34m | |\\  | |____| | \\ \\  \\  /  | |____ ")
+	fmt.Println("\u001B[34m |_| \\_|______|_|  \\_\\  \\/   |______|")
+	fmt.Println("")
+	fmt.Println("")
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+		return
+	}
+	defer listener.Close()
+	fmt.Printf("Listen to :%d\n", port)
+	go readConsoleCommands()
+
+	for {
+		clientConn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting client connection:", err)
+			continue
+		}
+
+		go handleConnection(clientConn, minecraftServer, minecraftPort)
+		fmt.Println("Ein Client ist gejoint", clientConn.RemoteAddr().String())
+	}
+}
